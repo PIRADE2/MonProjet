ac8e32a26b5e72690a9f00ce7f744cabd6a79be9
PIRADE2 <epeenaert@gmail.com>
2022-12-28 16:13:03 +0100

    version 2

--- chapitre_include.php
+++ chapitre_include.php
@@ -2,7 +2,7 @@
 <html> 
 	<head>
 		<meta charset="utf-8" />
-		<title>Apprendre le include</title> 
+		<title>Apprendre le include php</title> 
 		<link rel="stylesheet" type="text/css" href="style.css">
 	</head>
 <body>
